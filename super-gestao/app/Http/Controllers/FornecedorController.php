77bd7bff842c42f307725973ca7948680a61f8f9
Paulo Corazza <paulocorazza@Paulos-MacBook-Pro.local>
2022-01-09 18:48:14 -0300

    foreach e forelse

--- super-gestao/app/Http/Controllers/FornecedorController.php
+++ super-gestao/app/Http/Controllers/FornecedorController.php
@@ -9,24 +9,27 @@ class FornecedorController extends Controller
     public function index()
     {
         $fornecedores = [
-        0 => ['nome' => 'fornecedor1', 
+        0 => ['nome' => 'Malaquias Máquinas', 
                  'status' => 'N',
-                 'cnpj' => 'asd',
+                 'cnpj' => '37.431.456.507/0001-00',
                  'ddd' => '11',
                  'telefone' => '00000-0000'
         ],
-        1 => ['nome' => 'Fornecedor 2',
+        1 => ['nome' => 'Biridin Superfícies Rígidas',
                 'status' => 'S',
                 'ddd' => '32',
-                'cnpj' => 'asd',
+                'cnpj' => '37.431.456.507/0001-00',
                 'telefone' => '00000-0000'
             ],
-        2 => ['nome' => 'Fornecedor 3',
+        2 => ['nome' => 'Power Guido Perfurações',
                 'status' => 'S',
                 'ddd' => '32',
-                'cnpj' => 'asd',
+                'cnpj' => '37.431.456.507/0001-00',
                 'telefone' => '00000-0000'
         ]];
+
+        //declaração vazia para forelse
+        //$fornecedores = [];
         
         return view('app.fornecedor.index',compact('fornecedores'));
     }
--- super-gestao/resources/views/app/fornecedor/index.blade.php
+++ super-gestao/resources/views/app/fornecedor/index.blade.php
@@ -2,8 +2,8 @@
 
 
 @isset($fornecedores)
-    @foreach($fornecedores as $indice => $fornecedor)
-    <label for="fornecedores">Fornecedor</label>    
+    {{-- @foreach($fornecedores as $indice => $fornecedor)
+        <label for="fornecedores">Fornecedor</label>    
         <p>{{ $fornecedor['nome'] }}</p>
         <label for="status">Status</label>
         <p>{{ $fornecedor['status'] }}</p>
@@ -12,7 +12,35 @@
         <label for="cnpj">CNPJ</label>
         <p>{{ $fornecedor['cnpj'] }}</p>
         <hr>
-    @endforeach
+    @endforeach --}}
+
+    <table>
+        <tr>
+            <th>Fornecedor </th>
+            <th> Status</th>
+            <th>Telefone</th>
+            <th>CNPJ</th>
+        </tr>
+        @forelse($fornecedores as $indice => $fornecedor)
+        <tr>
+            <td>
+                {{ $fornecedor['nome'] }} 
+            </td>
+            <td>
+                {{ $fornecedor['status'] }}
+            </td>
+            <td>
+                {{ $fornecedor['telefone'] }}
+            </td>
+            <td>
+                {{ $fornecedor['cnpj'] }}
+            </td>
+        </tr>
+    </table>
+  
+    @empty
+    Não existem fornecedores cadastrados 
+    @endforelse
 @endisset 
 
     
