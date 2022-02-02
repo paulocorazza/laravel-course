b3df32c5c384a8c3f1edc015f1c93e6660ffa360
“Paulo <corazza.paulovinicius@gmail.com>
2022-02-02 20:15:07 -0300

    fixes

--- blog/resources/views/site/layouts/_partials/topo.blade.php
+++ blog/resources/views/site/layouts/_partials/topo.blade.php
@@ -5,8 +5,8 @@
 
     <div class="menu">
         <ul>
-            <li><a href="{{ route('site.index') }}">Principal</a></li>
-            <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+            <li><a href="{{ route('site.principal') }}">Principal</a></li>
+            <li><a href="{{ route('site.sobre-nos') }}">Sobre Nós</a></li>
             <li><a href="{{ route('site.contato') }}">Contato</a></li>
         </ul>
     </div>
